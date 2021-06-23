63b0e7a43e8680f33e02d25d3d8a82ac93a8fd49
Piyush Sharma <piyush.sharma@team.telstra.com>
2021-06-23 18:45:20 +1000

    [Piyush] - initial changes to consume component1 from module-federation1 project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Component1 from 'remoteComponent/Component1';
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+    <div>
+        <Component1/>
+        In component 2, the content from component1 above
+    </div>);
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App/>, document.getElementById("app"));
